0a772e3dfdc2e78a20ad3cbd513af5a33a80ebeb
ninz0018 <ninzgetil25@gmail.com>
2024-03-01 02:25:33 -0800

    mesa

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function clk() {
+alert("Invalid Signin");
+}
+function sig() {
+alert("Invalid Signup");
+}
+function forget() {
+    alert("Do you want to continue?");
+}
+function pasok() {
+    alert("Successfully Login");
+}
+function ign() {
+    alert("Loading");
+}
