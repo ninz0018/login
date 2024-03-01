d947451264b6a7e538ce9d7a11fb6afb4eba2b7b
ninz0018 <ninzgetil25@gmail.com>
2024-03-01 22:45:11 +0800

    message

--- javascript.js
+++ javascript.js
@@ -1,15 +1,9 @@
-function clk() {
-alert("Invalid Signin");
-}
-function sig() {
-alert("Invalid Signup");
-}
-function forget() {
-    alert("Do you want to continue?");
-}
-function pasok() {
-    alert("Successfully Login");
-}
-function ign() {
-    alert("Loading");
+function clk(val) {
+    let buttons = document.querySelectorAll('.action'); // Get all buttons with the 'action' class
+    buttons.forEach(button => { // Iterate through each button
+        if (button !== val) { // Check if the button is not the clicked one
+            button.classList.remove("active"); // Remove the 'active' class from the button
+        }
+    });
+    val.classList.toggle("active");
 }
