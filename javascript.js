96bc26c8d0a6dd0307da627069d5d959ee43b7a8
ninz0018 <ninzgetil25@gmail.com>
2024-03-02 19:58:46 +0800

    message

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 function validateForm() {
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-    
+   
     if (username === "your_username" && password === "your_password") {
         alert("Login successful!");
         return true; 
+    } else {
         alert("Invalid username or password. Please try again.");
         return false; 
     }
